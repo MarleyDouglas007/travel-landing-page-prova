4d08b233789f06b17c59e5c0f649f47b88c40cc2
MarleyDouglas007 <marley.santos2308@gmai.com>
2023-10-19 16:39:57 -0300

    hhh

--- app/page.js
+++ app/page.js
@@ -1,7 +1,32 @@
 import styles from './page.module.css'
+import Image from 'next/image'
+import hawaii from '../public/Hawaii.png'
+import seta from '../public/arrow-right.svg'
+
+function Botao({x,y}) {
+  return (
+    <button style= {{left:x,top:y}} className={styles.botao}>Start</button>
+  )
+}
+
+function Card({localidade,imagem}){
+  return(
+    <div className={styles.card}>
+<p className={styles.localidade}>{localidade}</p>
+<Image src={seta}/>
+<Image src={imagem}/>
+    </div>
+  )
+} 
 
 export default function Home() {
   return (
-    <h1></h1>
+  <>
+   <Botao x={135} y={601.72}/>    
+   <Card localidade="Hawaii"imagem={hawaii}/>
+   <Card localidade="Cyprus"imagem={hawaii}/>
+   <Card localidade="Bali"imagem={hawaii}/>
+   
+  </>
   )
 }
